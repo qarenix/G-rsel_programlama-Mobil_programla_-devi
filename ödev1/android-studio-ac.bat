a104a6bf6b8bff11ca7b677a4171ab74a02ed993
enes özdemir <168411020+qarenix@users.noreply.github.com>
2026-05-19 20:00:59 +0300

    Delete ödev1 directory

--- ödev1/android-studio-ac.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "STUDIO=C:\Program Files\Android\Android Studio\bin\studio64.exe"
-if not exist "%STUDIO%" (
-  echo Android Studio bulunamadi: %STUDIO%
-  pause
-  exit /b 1
-)
-start "" "%STUDIO%" "%~dp0"
